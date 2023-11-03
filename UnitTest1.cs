825a24d14f54b80c875f730ecbb1c403eca0ae79
Prasanta113 <149783699+Prasanta113@users.noreply.github.com>
2023-11-03 18:45:35 +0530

    Add files via upload

---
+++ UnitTest1.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using FluentAssertions;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //Arrange 
+            int firstNo = 8; int secondNo = 4;
+
+            //Act 
+
+            bool result = firstNo > secondNo;
+
+            Assert.IsTrue(result);
+
+            firstNo.Should().BeGreaterThan(secondNo);
+
+
+        }
+    }
+}
